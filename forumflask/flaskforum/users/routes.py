cd54d120430839def6dc952ed5734c0b11cdf809
shiyue zhang <zhangshiyuez@gmail.com>
2019-03-29 11:47:51 -0500

    Update user account

--- forumflask/flaskforum/users/routes.py
+++ forumflask/flaskforum/users/routes.py
@@ -1,15 +1,34 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_required, login_user, current_user, logout_user
-from flaskforum.users.forms import LoginForm, RegisterForm
+from flaskforum.users.forms import LoginForm, RegisterForm, UpdateAccountForm
 from flaskforum.replies.forms import ReplyForm
 from flaskforum import db, bcrypt
 from flaskforum.models import User, Post, Vote, Reply
+import os
+import secrets
+from PIL import Image
 
 users = Blueprint('users', __name__)
 
 @users.route("/account", methods=['GET', 'POST'])
+@login_required
 def account():
-	return render_template('account.html', title = 'Account')
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		if form.picture.data:
+			picture_file = save_picture(form.picture.data)
+			current_user.image_file = picture_file
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your account has been updated!', 'success')
+		return redirect(url_for('users.account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+	image_file = url_for('static', filename='profiles/' + current_user.image_file)
+	return render_template('account.html', title = 'Account',
+		image_file=image_file, form=form)
 
 @users.route("/register", methods=['GET', 'POST'])
 def register():
@@ -72,3 +91,17 @@ def my_likes():
 	replies = Reply.query.all()
 	return render_template('my_posts.html', title = 'My Likes', posts = posts, 
 		type = 'like',votes = votes, form = form, replies = replies)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profiles', picture_fn)
+
+    output_size = (50, 50)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
